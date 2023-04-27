642a8736efceaabe87ecbecd4e7cd2333cabfe90
specialgirl785 <118490981+specialgirl785@users.noreply.github.com>
2023-04-26 20:39:12 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,17 @@
+<head>
+    
+const firebaseConfig = {
+    apiKey: "AIzaSyDqz0BDEGzJfcBfEW8qhFvg_2OTKGvm_DI",
+    authDomain: "project-100-3a989.firebaseapp.com",
+    databaseURL: "https://project-100-3a989-default-rtdb.firebaseio.com",
+    projectId: "project-100-3a989",
+    storageBucket: "project-100-3a989.appspot.com",
+    messagingSenderId: "457286110342",
+    appId: "1:457286110342:web:33b76cc3dd1ad287ded507",
+    measurementId: "G-XNH8XLHBL1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  </head>
